e4791d5c90b76cc2e441f7e38e45656f3d68d678
Alexander-Guacan <adguacan@espe.edu.ec>
2024-08-31 11:08:12 -0500

    Add logo and about us link

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,10 +1,37 @@
+import { Link } from 'react-router-dom'
+import logo from './../assets/espe.png'
 import './NavBar.css'
 
 export function NavBar() {
+  const pages = [
+    {
+      url: "/about_us",
+      text: "Acerca de"
+    },
+  ]
 
   return (
     <header className="navbar">
-      <h1>Chabot-ESPE</h1>
+      <div>
+        <Link className='link' to={'/'}>
+          <img className='img logo' src={logo} alt="espe logo" />
+        </Link>
+      </div>
+
+      <h1 className='title'>Chatbot-ESPE</h1>
+
+      <nav>
+        <ul className='menu'>
+          {
+            pages.map((page, index) => (
+              <li key={index}>
+                <Link className='link' to={page.url} >{page.text}</Link>
+              </li>
+            ))
+          }
+        </ul>
+      </nav>
     </header>
   )
+
 }
